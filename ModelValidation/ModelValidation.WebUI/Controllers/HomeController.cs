c1df1071e73d130c1e8bed73219a8b26b2f17e84
Baki Öztürk <22.exe.94@gmail.com>
2020-01-18 12:58:36 +0300

    2020-01-18

---
+++ ModelValidation/ModelValidation.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModelValidation.WebUI.Models;
+
+namespace ModelValidation.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Register",new Register() {BirthDate=DateTime.Now });
+        }
+        public IActionResult Register(Register model)
+        {
+            if (string.IsNullOrEmpty(model.UserName))
+            {
+                ModelState.AddModelError(nameof(model.UserName), "Hata Mesajı 1");
+            }
+            return View("Completed", model);
+        }
+    }
+}
---
+++ ModelValidation/ModelValidation.WebUI/Models/Register.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelValidation.WebUI.Models
+{
+    public class Register
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        [UIHint("Date")]//BirthDate in datesi lazım sadece
+        public DateTime BirthDate { get; set; }
+        public bool TermsAccepted { get; set; }
+        //Kullanım koşullarını kabul etme
+    }
+}
